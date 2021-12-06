e30df35577562c3619b0bebbbbbf1aa1ef25c56f
rlaigu@tktk.ee <rlaigu@tktk.ee>
2021-12-06 11:34:57 +0200

    add new project

---
+++ ShoppingList/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace ShoppingListCreate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootDirectory = @"C:\Users\...\sample";
+            Console.WriteLine("Enter directory name:");
+            string newDirectory = Console.ReadLine();
+            Console.WriteLine("Enter file mane:");
+            string fileName = Console.ReadLine();
+
+
+            if (Directory.Exists($"{rootDirectory}\\{newDirectory}") && File.Exists($"{rootDirectory}\\{newDirectory}\\{fileName}"))
+            {
+                Console.WriteLine($"Directory and File exists.");
+            }
+            else
+            {
+                Directory.CreateDirectory($"{rootDirectory}\\{newDirectory}");
+                File.Create($"{rootDirectory}\\{newDirectory}\\{fileName}");
+            }
+
+        }
+    }
+}
---
+++ WishList/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic; //pakett, mis oskab listidega tööd teha
+
+
+namespace WishList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> myWishList = new List<string>();
+
+            Console.WriteLine(myWishList.Count);
+
+            myWishList.Add("coffee machine");
+            myWishList.Add("new pair of socks");
+            myWishList.Add("box of chocolates");
+            myWishList.Add("new pillow");
+
+            Console.WriteLine(myWishList.Count);
+
+            foreach(string wish in myWishList)
+            {
+            Console.WriteLine(wish);
+            }
+            
+           
+
+        }
+    }
+}
